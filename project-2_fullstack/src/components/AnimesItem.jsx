fff1b1caef1b6d2d793e987a94ad1443b3a34e3f
MauricioJrB <mjunior1205@gmail.com>
2024-04-25 20:58:20 -0300

    feat: update to render Anime component

--- project-2_fullstack/src/components/AnimesItem.jsx
+++ project-2_fullstack/src/components/AnimesItem.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const AnimesItem = ({anime}) => {
-   return (
-      <div className="animes-item">
-         <p>Anime: {anime.anime}</p>
-         <p>Character: {anime.character}</p>
-         <p>Quote: {anime.quote}</p>
-      </div>
-   );
+import "./AnimesItem.scss";
+
+const AnimesItem = ({ animeData }) => {
+ 
+  if (!animeData || animeData.length === 0) {
+    return <p>Aparecerá aqui...</p>
+  }
+  return (
+    <div className="animes-item">
+      <ul className="list-item">
+        <li><p>Personagem: {animeData.character}</p></li>
+        <li><p>Anime: {animeData.anime}</p></li>
+        {animeData.quotes.map((quote, index) => (
+          <li key={index}>
+            <p> Citação {index + 1} - {quote}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AnimesItem;
